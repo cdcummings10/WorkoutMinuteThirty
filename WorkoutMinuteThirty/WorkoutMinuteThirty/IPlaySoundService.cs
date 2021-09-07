f17f35c1462eb13431caa78061b873331d0e7831
lurkor <lurkortv@gmail.com>
2021-09-07 14:53:26 -0700

    Add project files.

---
+++ WorkoutMinuteThirty/WorkoutMinuteThirty/IPlaySoundService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkoutMinuteThirty
+{
+    interface IPlaySoundService
+    {
+        void PlaySystemSound();
+    }
+}
---
+++ WorkoutMinuteThirty/WorkoutMinuteThirty/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WorkoutMinuteThirty
+{
+    public partial class MainPage : ContentPage
+    {
+        Stopwatch stopwatch;
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            stopwatch = new Stopwatch();
+            lblStopwatch.Text = "00:00:00";
+        }
+
+        private void btnStart_Clicked(object sender, EventArgs e)
+        {
+            stopwatch.Start();
+            Device.StartTimer(TimeSpan.FromMilliseconds(100), () =>
+            {
+                lblStopwatch.Text = stopwatch.Elapsed.ToString();
+                return true;
+            }
+            );
+        }
+
+        private void btnStop_Clicked(object sender, EventArgs e)
+        {
+            stopwatch.Stop();
+        }
+
+        private void btnReset_Clicked(object sender, EventArgs e)
+        {
+            stopwatch.Reset();
+        }
+    }
+}
